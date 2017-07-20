506c011612aa2b5f3f4753a285668120c5ec436c
yasirhussain1 <30301982+yasirhussain1@users.noreply.github.com>
2017-07-19 20:31:23 -0700

    Add files via upload

---
+++ media1.py
@@ -0,0 +1,8 @@
+import webbrowser
+class Movie():
+    def __init__(self, movie_title, story_line, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        
