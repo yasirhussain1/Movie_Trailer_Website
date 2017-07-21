cf1e8992e6b3168a3eed5ad8bfdf8a25a6e6add3
yasirhussain1 <30301982+yasirhussain1@users.noreply.github.com>
2017-07-21 11:56:05 -0700

    Update media1.py

--- media1.py
+++ media1.py
@@ -1,8 +1,10 @@
 import webbrowser
+
+
 class Movie():
     def __init__(self, movie_title, story_line, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        
+
